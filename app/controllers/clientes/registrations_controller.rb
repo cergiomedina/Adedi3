dc5ec687e7de8f1fffeeef9806a3daafe4d5f725
Sergio <medina159@gmail.com>
2016-02-16 21:09:42 -0300

    NavBar mejorada

--- app/controllers/clientes/registrations_controller.rb
+++ app/controllers/clientes/registrations_controller.rb
@@ -68,7 +68,7 @@ class Clientes::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
    def after_sign_up_path_for(resource)
-     disfrazs_path
+     redirect_to disfrazs_path
    end
 
   # The path used after sign up for inactive accounts.
