40663d9d0adce849ca8958cde164f9a405a5fa7c
Sergio <medina159@gmail.com>
2016-02-19 19:21:45 -0300

    cambio en disfraces

--- app/models/disfraz.rb
+++ app/models/disfraz.rb
@@ -1,10 +1,10 @@
 class Disfraz < ActiveRecord::Base
 	self.table_name = "disfraz"
 
-		has_attached_file :imagen, styles: { medium: "300x300>", thumb: "100x100>" , big: "500x500>"}, default_url: "default.png"
-  validates_attachment_content_type :imagen, content_type: /\Aimage\/.*\Z/
-		belongs_to :categoria, class_name: "categoria"
-		validates_attachment_presence :imagen, :message => "El disfraz debe tener una imagen representativa."
+	has_attached_file :imagen, styles: { medium: "300x300>", thumb: "100x100>" , big: "500x500>"}, default_url: "default.png"
+  	validates_attachment_content_type :imagen, content_type: /\Aimage\/.*\Z/
+	belongs_to :categoria, class_name: "categoria"
+	validates_attachment_presence :imagen, :message => "El disfraz debe tener una imagen representativa."
 	validates_attachment_size :imagen, :less_than => 5.megabytes
 	
 	validates_presence_of   :CATEGORIA_DISFRAZ, :message => "El disfraz debe poseer un nombre"
