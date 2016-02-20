8c6c23ccd27974c8a55454a381f9d9179d0219c2
rmendozahuircapan <rodrigo.mendoza@usach.cl>
2016-02-20 04:26:56 -0300

    validacion entradas new disfraz (excepto categoria)

--- app/models/disfraz.rb
+++ app/models/disfraz.rb
@@ -6,7 +6,11 @@ class Disfraz < ActiveRecord::Base
 	belongs_to :categoria, class_name: "categoria"
 	validates_attachment_size :imagen, :less_than => 5.megabytes
 	
-	
+	validates_presence_of   :NOMBRE_DISFRAZ, :message => "El disfraz debe poseer un nombre"
+	#validates_presence_of   :CATEGORIA_DISFRAZ, :message => "El disfraz debe poseer una categoría"
+    validates_presence_of   :STOCK_DISFRAZ, :message => "El disfraz debe tener al menos 1 prenda disponible"
+    validates_presence_of   :ID_CATEGORIA, :message => "El disfraz debe pertenecer a una categoría"
+
 	def self.search(term)
 	   where("CATEGORIA_DISFRAZ like :term", term: "#{term}")
 	 end
