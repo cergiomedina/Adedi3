8f5ebf641e9fc9f7a3a96598e52ec9836f22c018
rmendozahuircapan <rodrigo.mendoza@usach.cl>
2016-02-21 02:00:12 -0300

    agregué linea que había borrado

--- app/models/disfraz.rb
+++ app/models/disfraz.rb
@@ -4,6 +4,7 @@ class Disfraz < ActiveRecord::Base
 	has_attached_file :imagen, styles: { medium: "300x300>", thumb: "100x100>" , big: "500x500>"}, default_url: "default.png"
   	validates_attachment_content_type :imagen, content_type: /\Aimage\/.*\Z/
 	belongs_to :categoria, class_name: "categoria"
+	#validates_attachment_presence :imagen, :message => "El disfraz debe tener una imagen representativa."
 	validates_attachment_size :imagen, :less_than => 5.megabytes
 	
 	validates_presence_of   :NOMBRE_DISFRAZ, :message => "El disfraz debe poseer un nombre"
