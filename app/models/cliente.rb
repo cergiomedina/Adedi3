6a02ccf91ba286c759163ded5c8953e0258c37a7
Sergio <medina159@gmail.com>
2016-02-27 02:04:14 -0300

    mejorado el carrito de arriendos. Solo 1 cliente por rut

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -4,14 +4,15 @@ class Cliente < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 		self.table_name = "cliente"
+
       validates_presence_of   :NOMBRE_CLIENTE
       validates_presence_of   :DIRECCION_CLIENTE
-      validates_presence_of   :RUT_CLIENTE
+      validates_presence_of   :RUT_CLIENTE, uniqueness: true, :message => "Ya existe un cliente con ese rut"
       validates_presence_of   :TELEFONO_CLIENTE
       has_many :pedidos
       has_many :arriendos, dependent: :destroy
       
-      validates :RUT_CLIENTE, rut: true
+      validates :RUT_CLIENTE, rut: true, uniqueness: true
       
       has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", big: "200x200>" }, default_url: "default.png"
       validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
